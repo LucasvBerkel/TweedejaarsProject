ce3f72bb8316296b113e9709e4268639e62595ca
rien333 <rien3334@gmail.com>
2016-07-19 19:19:26 +0200

    Updated compilation instructions to be cross platform

--- SF_Cairo/RS.c
+++ SF_Cairo/RS.c
@@ -1,5 +1,5 @@
-// OS X compilation:
-/* gcc -Wall -g  myvars.c TCOL.c DE.c HM.c RS.c -I/usr/local/include/cairo -L/usr/local/lib/ -lcairo `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`  -o RS -Wno-dangling-else -Wno-switch;
+// Compilation (cross platform!):
+/* gcc -Wall -g  myvars.c TCOL.c DE.c HM.c RS.c -lm `pkg-config --cflags cairo` `pkg-config --libs cairo` `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`  -o RS -Wno-dangling-else -Wno-switch; */
 
 /* test graphics 21.2.90 18:00
             definitions */
