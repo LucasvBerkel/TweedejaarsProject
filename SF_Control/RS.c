0b17aca85463bf2d6b6d938e3444440a3c43cfe3
rien333 <rien334@gmail.com>
2016-12-08 16:50:12 +0100

    Improved drawing code

--- SF_Control/RS.c
+++ SF_Control/RS.c
@@ -246,7 +246,7 @@ int main(int argc, char *argv[])
 	Initialized_Graphics = 0;
 
 // Basic GTK initialization
- 	GtkWidget *window;
+ 	 
   GtkWidget *darea;
   gtk_init(&argc, &argv);
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
