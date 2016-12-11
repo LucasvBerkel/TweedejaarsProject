56be77c2c21be19aeacd240c63922bc1cc01f5f6
rien333 <rien334@gmail.com>
2016-12-11 15:47:38 +0100

    debug

--- GoogleDQN/record_game.sh
+++ GoogleDQN/record_game.sh
@@ -39,7 +39,7 @@ ncols=1
 agent_params="lr="$lr",ep=1,ep_end="$eps_end",ep_endt="$eps_endt",discount="$discount",hist_len=4,learn_start="$learn_start",replay_memory="$replay_memory",update_freq="$update_freq",n_replay="$n_replay",network="$netfile",preproc="$preproc_net",state_dim="$state_dim",minibatch_size=32,rescale_r=1,ncols="$ncols",bufferSize=512,valid_size=500,target_q=10000,clip_delta=1,min_reward=-1,max_reward=1"
 
 
-gpu=-1
+gpu=0
 random_starts=5
 pool_frms="type="$pool_frms_type",size="$pool_frms_size
 
